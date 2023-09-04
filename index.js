755aef563ae08bde69e6fbc376ca30027c3c35ee
tuanvu <robin.github@gmail.com>
2023-09-04 16:45:01 +0800

    feat: code change

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const glob = require('@actions/glob');
+const patterns = ['**/application-*.yaml', '**/application-*.properties']
 
 try {
 
   const configServerUrl = core.getInput('config-server-url');
   const workspace = core.getInput('workspace')
   console.log(`migrate from ${ workspace } config server: ${configServerUrl}!`);
+
+  const globber = glob.create(patterns.join('\n'))
+  const files =  globber.glob()
+  files.forEach((item, i) => {
+    console.log(item)
+  });
+
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
